47594fee23f0154da31dbd4abbac32bc03258afd
James Ginns <starvagrant@yahoo.com>
2019-05-29 18:36:37 -0500

    Remove hard coded vocab list

--- vocabulario.py
+++ vocabulario.py
@@ -2,10 +2,6 @@
 import cmd
 import random
 
-worddict = { "merced": "mercy", "torneo": "tournament" , "tatarabuela": "great great grandmother", "vergüenza":  "shame", "mortaja":  "shroud" , "tejer":  "to knit", "barbero":  "barber", "instead": "en lugar", "apartment": "apartamento", "solution": "solución", "Budget": "presupuesto", "quebrantar":  "to break", "bodega":  "cellar", "cotidianidad":  "everydayness", "ataúd":  "coffin", "jubilado":  "retired", "sudar":  "to sweat", "deprimido":  "depressed", "jubilado":  "retired", "sudar":  "to sweat", "deprimido":  "depressed", "asombrarse":  "to wonder", "asustar":  "to frighten" , "asunto":  "affair", "breathe": "respirar", "previous": "anterior", "pesarices":  "regret", "parranda":  "spree", "pregonar":  "preach", "pergamino":  "parchment", "diluvio":  "flood", "goal": "objetivo"}
-
-
-
 class SpanishCmd(cmd.Cmd):
     prompt = '\n\033[0mEspañol> '
 
@@ -25,16 +21,6 @@ class SpanishCmd(cmd.Cmd):
                 else:
                     reading=False
                 i=i+1
-
-    def do_words(self, args):
-        for key in worddict:
-            inputword = key + "? "
-            player_input = input(inputword)
-            if (player_input.lower() == worddict[key]):
-                print('True')
-            else:
-                print('False')
-                print(worddict[key])
 
     def do_palabras(self, args):
         random.shuffle(self.wordlist)
