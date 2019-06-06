7faa02b2c2c6cc9df3a7b78c4539d3e5b2ff9697
James Ginns <starvagrant@yahoo.com>
2019-06-06 14:45:39 -0500

    Write test for word list loading function
    Test that the word list loading function works as designed:
    that it goes through a file and parses entries into lists of
    lists.

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 
 import unittest
 import vocabulario
+import os
 
 class Tests(unittest.TestCase):
 
@@ -12,5 +13,18 @@ class Tests(unittest.TestCase):
         loop = vocabulario.SpanishCmd()
         self.assertEqual(loop.keymap(user_string1),target_string)
         self.assertEqual(loop.keymap(user_string2),target_string)
+
+    def test_load_word_list(self):
+        if os.path.exists('test_dir/test_list.csv'):
+            os.remove('test_dir/test_list.csv')
+            os.rmdir('test_dir/')
+        os.mkdir('test_dir')
+        with open('test_dir/test_list.csv', 'w') as f:
+            f.write('answer	repuesta\n')
+
+        loop = vocabulario.SpanishCmd()
+        loop.load_word_list('test_dir/test_list.csv')
+        expected = [['answer','repuesta']]
+        self.assertListEqual(loop.wordlist, expected)
 
 unittest.main()
