011296c4b8d769935b45915fcfbe415cdf5e9f55
James Ginns <starvagrant@yahoo.com>
2019-06-07 09:30:36 -0500

    Refactor Tests for easier cleanup
    Create a static function to clean the test directory of temporary
    files, and call it for every test involving temporary files.

--- tests.py
+++ tests.py
@@ -4,8 +4,12 @@ import unittest
 import vocabulario
 import os
 
-class Tests(unittest.TestCase):
+def clean_test_dir(directory='test_dir'):
+        scan = os.scandir('test_dir')
+        for filename in scan:
+            os.remove(filename)
 
+class Tests(unittest.TestCase):
     def test_keymap(self):
         user_string1= """'a'e'i'o'u:u~n!!??"""
         user_string2= """"a"e"i"o"u:u~n!!??"""
@@ -15,10 +19,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(loop.keymap(user_string2),target_string)
 
     def test_load_word_list(self):
-        if os.path.exists('test_dir/test_list.csv'):
-            os.remove('test_dir/test_list.csv')
-            os.rmdir('test_dir/')
-        os.mkdir('test_dir')
+        clean_test_dir()
         with open('test_dir/test_list.csv', 'w') as f:
             f.write('answer	repuesta	0')
 
@@ -28,10 +29,7 @@ class Tests(unittest.TestCase):
         self.assertListEqual(loop.wordlist, expected)
 
     def test_write_word_list(self):
-        if os.path.exists('test_dir/test_list.csv'):
-            os.remove('test_dir/test_list.csv')
-            os.rmdir('test_dir/')
-        os.mkdir('test_dir')
+        clean_test_dir()
         with open('test_dir/test_list.csv', 'w') as f:
             f.write('answer	repuesta	0')
 
