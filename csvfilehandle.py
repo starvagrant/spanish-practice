fd7edc4b89924aeb6484eab4898d0e80cbc8f651
James Ginns <starvagrant@yahoo.com>
2019-06-16 00:44:05 -0500

    Alter CsvFileHandle class to only be a file handle
    Have CsvFileHandle's only responsibility be to return list of
    OrderedDicts from csv files, and writing lists of OrderDicts
    into csv files.

--- csvfilehandle.py
+++ csvfilehandle.py
@@ -5,33 +5,35 @@ from collections import OrderedDict
 
 class CsvFileHandle():
 
-    def __init__(self,file_name=''):
+    def __init__(self,file_name):
         self.file_name=file_name
-        self.word_list = []
 
     def read(self, file_name=''):
+        word_list = []
         if file_name == '':
             file_name=self.file_name
         with open(file_name, 'r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             for row in csv_reader:
-                self.word_list.append(row)
+                word_list.append(row)
 
-    def write(self, file_name=''):
+        return word_list
+
+    def write(self, word_list, file_name=''):
         if file_name == '':
             file_name=self.file_name
         with open(file_name, 'w') as csv_file:
-            if len(self.word_list) == 0:
+            if len(word_list) == 0:
                 print('Nothing to Write')
                 return
 
             fieldnames = []
-            for key in self.word_list[0].keys():
+            for key in word_list[0].keys():
                 fieldnames.append(key)
 
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
-            for entry in self.word_list:
+            for entry in word_list:
                 print(entry)
                 writer.writerow(entry)
 
