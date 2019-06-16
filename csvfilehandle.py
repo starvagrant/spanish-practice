a008c5684e49682bfe17b5d13dffbaf525612eab
James Ginns <starvagrant@yahoo.com>
2019-06-16 00:29:36 -0500

    Move write/read list functions to read/write functions
    Name functions as described in the tests. The read function reads
    a csv file, the write function writes one. Tests now fail due to
    AssertionErrors, rather than mistakes in class/syntax.

--- csvfilehandle.py
+++ csvfilehandle.py
@@ -1,42 +1,26 @@
 #!/usr/bin/env python3
 
 import csv
+from collections import OrderedDict
 
 class CsvFileHandle():
 
-    def __init__(self,file_name='example.csv'):
+    def __init__(self,file_name=''):
         self.file_name=file_name
         self.word_list = []
 
-    def read(self, file_name='example.csv'):
-        with open('example.csv', mode='r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            line_count = 0
-            for row in csv_reader:
-                if line_count == 0:
-                    print(f'Column names are {", ".join(row)}')
-                    line_count += 1
-                print(f'\t{row["spanish"]} means "{row["english"]}" in English. You\'ve guessed this {row["correct_guesses"]} times')
-                line_count += 1
-                print(f'Processed {line_count} lines.')
-
-    def write(self, file_name='example.csv'):
-        with open(file_name, mode='w') as csv_file:
-            fieldnames = ['spanish', 'english', 'correct_guesses']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-            writer.writeheader()
-            writer.writerow({'spanish': 'ayudar', 'english': 'to help', 'correct_guesses': '0'})
-            writer.writerow({'spanish': 'buscar', 'english': 'to search', 'correct_guesses': '0'})
-
-    def read_in_list(self, file_name='example.csv'):
-        with open('example.csv', 'r') as csv_file:
+    def read(self, file_name=''):
+        if file_name == '':
+            file_name=self.file_name
+        with open(file_name, 'r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             for row in csv_reader:
                 self.word_list.append(row)
 
-    def write_in_list(self, file_name='example.csv'):
-        with open('example.csv', 'w') as csv_file:
+    def write(self, file_name=''):
+        if file_name == '':
+            file_name=self.file_name
+        with open(file_name, 'w') as csv_file:
             if len(self.word_list) == 0:
                 print('Nothing to Write')
                 return
@@ -52,6 +36,6 @@ class CsvFileHandle():
                 writer.writerow(entry)
 
 if __name__ == '__main__':
-    csvfile = CsvFileHandle()
-    csvfile.read_in_list()
-    csvfile.write_in_list()
+    csvfile = CsvFileHandle('example.csv')
+    csvfile.read()
+    csvfile.write()
